106a26ffcaeb9d10ad7e7a971133f4606d27a965
Donkeyboy223 <sdoncaster5@gmail.com>
2024-03-02 14:37:02 +0000

    fixes

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@
 
 import Image from 'next/image'
 import './globals.css'
+import { Navigate } from "react-router-dom";
 import Navbar from './components/navbar'
 import Type from './components/type';
 import { motion } from 'framer-motion';
@@ -13,9 +14,9 @@ import { LampContainer } from './components/lamp';
 import { CardBody, CardContainer, CardItem } from './components/3dcard';
 import { Button } from './components/moving-border';
 import { PinContainer } from './components/3dpin';
+import Link from 'next/link';
 
 export default function Home() {
-
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -219,7 +220,7 @@ export default function Home() {
               <path d="M502.3 190.8c3.9-3.1 9.7-.2 9.7 4.7V400c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V195.6c0-5 5.7-7.8 9.7-4.7 22.4 17.4 52.1 39.5 154.1 113.6 21.1 15.4 56.7 47.8 92.2 47.6 35.7.3 72-32.8 92.3-47.6 102-74.1 131.6-96.3 154-113.7zM256 320c23.2.4 56.6-29.2 73.4-41.4 132.7-96.3 142.8-104.7 173.4-128.7 5.8-4.5 9.2-11.5 9.2-18.9v-19c0-26.5-21.5-48-48-48H48C21.5 64 0 85.5 0 112v19c0 7.4 3.4 14.3 9.2 18.9 30.6 23.9 40.7 32.4 173.4 128.7 16.8 12.2 50.2 41.8 73.4 41.4z"></path>
             </svg>
           </a>
-          <a href="https://github.com/donkeyboy223" class="bg-[#4E4E50] w-[40px] flex flex-col items-center justify-center h-[40px] rounded-full" aria-label="Visit My GitHub Profile">
+          <a href="https://github.com/samdonks" class="bg-[#4E4E50] w-[40px] flex flex-col items-center justify-center h-[40px] rounded-full" aria-label="Visit My GitHub Profile">
             <svg stroke="currentColor" fill="currentColor" xmlns="http://www.w3.org/2000/svg" width="24" height="24" class="text-white" viewBox="0 0 24 24">
               <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.840 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.430.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"></path>
             </svg>
@@ -360,8 +361,10 @@ export default function Home() {
       >
         <div className="flex basis-full flex-col tracking-tight text-slate-100/50 sm:basis-1/2 w-[554px] h-[334px] ">
           <div className="flex flex-1 w-full rounded-lg ">
+            <Link href='https://wwww.visionbot.cx'>
             <img src="/assets/vision.png" alt="Project1" loading='lazy' />
             <source srcSet='/assets/vision.webp' type="image/webp"></source>
+            </Link>
           </div>
         </div>
       </PinContainer>
@@ -388,8 +391,10 @@ export default function Home() {
               >
                 <div className="flex basis-full flex-col tracking-tight text-slate-100/50 sm:basis-1/2 w-[554px] h-[334px] ">
                   <div className="flex flex-1 w-full rounded-lg ">
-                    <img src="/assets/reflect.png" alt="Project2" loading='lazy' />
+                  <Link href='https://www.reflect.app'>
+                    <img src="/assets/reflecttest.png" class='' alt="Project2" loading='lazy' />
                     <source srcSet='/assets/reflect.webp' type="image/webp"></source>
+                  </Link>
                   </div>
                 </div>
               </PinContainer>
@@ -414,22 +419,24 @@ export default function Home() {
               transition={{ ...animationTransition, delay: 0.2 }}
             >
               <PinContainer
-                title="Eccomerce Store"
-                href="https://www.github.com/samdonks/EccomerceStore"
+                title="URL Shortener"
+                href="https://urlshortener-sigma-seven.vercel.app/"
               >
                 <div className="flex basis-full flex-col tracking-tight text-slate-100/50 sm:basis-1/2 w-[554px] h-[334px] ">
                   <div className="flex flex-1 w-full rounded-lg ">
-                    <img src="/assets/ecommerce.png" alt="Project2" loading='lazy' />
-                    <source srcSet='/assets/ecommerce.webp' type="image/webp"></source>
+                    <Link href="https://urlshortener-sigma-seven.vercel.app/">
+                    <img src="/assets/urlshortener.png" alt="Project2" loading="lazy" />
+                    <source srcSet="/assets/urlshortener.webp" type="image/webp" />
+                    </Link>
                   </div>
                 </div>
               </PinContainer>
-              <p class='text-[14px] font-bold text-[#A6ACBB] pt-10'>Ecommerce</p>
+              <p class='text-[14px] font-bold text-[#A6ACBB] pt-10'>URLShortener</p>
               <div class="flex flex-row gap-x-4 pt-1 pb-2">
-              <div class="bg-[#4E4E50]/20 text-[#4E4E50] text-[14px] px-2 py-1 rounded-md flex flex-col items-center hover:text-[#00ff66]">React</div>
+              <div class="bg-[#4E4E50]/20 text-[#4E4E50] text-[14px] px-2 py-1 rounded-md flex flex-col items-center hover:text-[#00ff66]">NextJS</div>
                 <div class="bg-[#4E4E50]/20 text-[#4E4E50] text-[14px] px-2 py-1 rounded-md flex flex-col items-center hover:text-[#00ff66]">NodeJS</div>
                 <div class="bg-[#4E4E50]/20 text-[#4E4E50] text-[14px] px-2 py-1 rounded-md flex flex-col items-center hover:text-[#00ff66]">MySQL</div>
-                <div class="bg-[#4E4E50]/20 text-[#4E4E50] text-[14px] px-2 py-1 rounded-md flex flex-col items-center hover:text-[#00ff66]">SimpleUI</div>
+                <div class="bg-[#4E4E50]/20 text-[#4E4E50] text-[14px] px-2 py-1 rounded-md flex flex-col items-center hover:text-[#00ff66]">Planetscale</div>
               </div>
             </motion.div>
           </div>
@@ -448,8 +455,10 @@ export default function Home() {
               >
                 <div className="flex basis-full flex-col tracking-tight text-slate-100/50 sm:basis-1/2 w-[554px] h-[334px] ">
                   <div className="flex flex-1 w-full rounded-lg ">
-                    <img src="/assets/spear.png" alt="Project2" loading='lazy' />
+                    <Link href='https://www.spearproxies.com'>
+                    <img src="/assets/spear.png" class='' alt="Project2" loading='lazy' />
                     <source srcSet='/assets/spear.webp' type="image/webp"></source>
+                    </Link>
                   </div>
                 </div>
               </PinContainer>
@@ -479,8 +488,10 @@ export default function Home() {
               >
                 <div className="flex basis-full flex-col tracking-tight text-slate-100/50 sm:basis-1/2 w-[554px] h-[334px] ">
                   <div className="flex flex-1 w-full rounded-lg ">
+                    <Link href='https://dashboard.lemonclub.io/dashboard#static'>
                     <img src="/assets/lemon.png" alt="Project2" loading='lazy' />
                     <source srcSet='/assets/lemon.webp' type="image/webp"></source>
+                    </Link>
                   </div>
                 </div>
               </PinContainer>
@@ -501,21 +512,22 @@ export default function Home() {
               transition={{ ...animationTransition, delay: 0.2 }}
             >
               <PinContainer
-                title="Pluto Proxies"
+                title="Ecommerce Store"
                 href=""
               >
                 <div className="flex basis-full flex-col tracking-tight text-slate-100/50 sm:basis-1/2 w-[554px] h-[334px] ">
                   <div className="flex flex-1 w-full rounded-lg ">
-                    <img src="/assets/pluto.png" alt="Project2" loading='lazy' />
-                    <source srcSet='/assets/pluto.webp' type="image/webp"></source>
+                    <img src="/assets/ecommerce.png" alt="Project2" loading='lazy' />
+                    <source srcSet='/assets/ecommerce.webp' type="image/webp"></source>
                   </div>
                 </div>
               </PinContainer>
-              <p class='text-[14px] font-bold text-[#A6ACBB] pt-10'>PlutoProxies</p>
+              <p class='text-[14px] font-bold text-[#A6ACBB] pt-10'>EcommerceStore</p>
               <div class="flex flex-row gap-x-4 pt-1">
                 <div class="bg-[#4E4E50]/20 text-[#4E4E50] text-[14px] px-2 py-1 rounded-md flex flex-col items-center hover:text-[#00ff66]">React</div>
-                <div class="bg-[#4E4E50]/20 text-[#4E4E50] text-[14px] px-2 py-1 rounded-md flex flex-col items-center hover:text-[#00ff66]">UI/UX</div>
-                <div class="bg-[#4E4E50]/20 text-[#4E4E50] text-[14px] px-2 py-1 rounded-md flex flex-col items-center hover:text-[#00ff66]">Figma</div>
+                <div class="bg-[#4E4E50]/20 text-[#4E4E50] text-[14px] px-2 py-1 rounded-md flex flex-col items-center hover:text-[#00ff66]">Simple UI</div>
+                <div class="bg-[#4E4E50]/20 text-[#4E4E50] text-[14px] px-2 py-1 rounded-md flex flex-col items-center hover:text-[#00ff66]">NodeJS</div>
+                <div class="bg-[#4E4E50]/20 text-[#4E4E50] text-[14px] px-2 py-1 rounded-md flex flex-col items-center hover:text-[#00ff66]">MySQL</div>
               </div>
             </motion.div>
           </div>
@@ -542,7 +554,7 @@ export default function Home() {
           <div class="flex flex-col items-start">
             <div class="flex flex-col items-start gap-y-2 mb-6">
               <h6 class="text-white animated-text text-[20px] font-bold">Get In Touch.</h6>
-              <p class="text-white text-[14px] max-w-[450px] text-left">Reach out to me if you need any help, have questions; our you are ready to start building your<span class="animated-text font-bold"> Dream Project.</span></p></div>
+              <p class="text-white text-[14px] max-w-[450px] text-left">Reach out to me if you need any help, have questions, or you are ready to start building your<span class="animated-text font-bold"> Dream Project.</span></p></div>
               <div class="flex flex-col items-start gap-y-2 mb-6">
                 <h6 class="text-white animated-text text-[12px] leading-loose">Mail</h6>
                 <p class="text-white text-[14px] max-w-[450px] text-left">sdoncaster5@gmail.com</p></div>
